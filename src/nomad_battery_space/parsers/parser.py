74a29a9559aa85d7ac88bb983051c0ecc0b22801
SvetiGrin <svetlana.grinman@helmholtz-berlin.de>
2025-11-24 10:19:17 +0100

    Fix plugin entry point after renaming to nomad_battery_space

--- src/nomad_battery_space/parsers/parser.py
+++ src/nomad_battery_space/parsers/parser.py
@@ -15,7 +15,7 @@ from nomad.datamodel.metainfo.workflow import Workflow
 from nomad.parsing.parser import MatchingParser
 
 configuration = config.get_plugin_entry_point(
-    'battery_space.parsers:parser_entry_point'
+    'nomad_battery_space.parsers:parser_entry_point'
 )
 
 
