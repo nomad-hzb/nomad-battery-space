ee73a9890a8b2e4b441f25fed5600ba067e0901a
SvetiGrin <svetlana.grinman@helmholtz-berlin.de>
2025-11-25 17:52:15 +0100

    Ruff linting fixes

--- src/nomad_battery_space/schema_packages/__init__.py
+++ src/nomad_battery_space/schema_packages/__init__.py
@@ -4,7 +4,9 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 class BatterySamplePackageEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
-        from nomad_battery_space.schema_packages.battery_sample_prototype import m_package
+        from nomad_battery_space.schema_packages.battery_sample_prototype import (
+            m_package,
+        )
 
         return m_package
 
