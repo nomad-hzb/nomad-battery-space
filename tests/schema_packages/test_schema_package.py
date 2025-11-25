8874a610a9e8c061d211dfaaa64c4b88ace3c007
SvetiGrin <svetlana.grinman@helmholtz-berlin.de>
2025-11-25 18:17:57 +0100

    Pytest fixes

--- tests/schema_packages/test_schema_package.py
+++ tests/schema_packages/test_schema_package.py
@@ -8,4 +8,7 @@ def test_schema_package():
     entry_archive = parse(test_file)[0]
     normalize_all(entry_archive)
 
-    assert entry_archive.data.message == 'Hello Markus!'
+    assert entry_archive.data.name == "bat_01"
+    assert entry_archive.data.components.anode.mass == 1.2
+    assert entry_archive.data.components.cathode.mass_active_material == 2.1
+    assert entry_archive.data.sample_identifiers.sample_id == "ABC123"
