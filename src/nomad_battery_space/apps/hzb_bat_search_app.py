78ed0e622592c6d9370b1d89e95cbdf83cb09e7d
Marcus Lewerenz <152180836+marlewe@users.noreply.github.com>
2026-06-04 10:40:05 +0200

    search: added filter for author

--- src/nomad_battery_space/apps/hzb_bat_search_app.py
+++ src/nomad_battery_space/apps/hzb_bat_search_app.py
@@ -1,18 +1,28 @@
-from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
+from nomad.config.models.ui import (
+    App,
+    Column,
+    Columns,
+    Menu,
+    MenuItemTerms,
+)
 
 hzb_bat_search_app = App(
     label='HZB Batteries Search App',
-    path='app',
+    path='search',
     category='battery space',
+    menu=Menu(
+        items=[
+            MenuItemTerms(
+                search_quantity='authors.name',
+                options=5,
+                show_input=True,
+            ),
+        ]
+    ),
     columns=Columns(
         selected=['entry_id'],
         options={
             'entry_id': Column(),
         },
-    ),
-    filter_menus=FilterMenus(
-        options={
-            'material': FilterMenu(label='Material'),
-        }
     ),
 )
