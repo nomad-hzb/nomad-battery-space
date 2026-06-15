0c3fc5f6ea8481ea1bf7db9e23981e1cc4566124
Marcus Lewerenz <152180836+marlewe@users.noreply.github.com>
2026-06-15 14:47:21 +0200

    search for type

--- src/nomad_battery_space/apps/hzb_bat_search_app.py
+++ src/nomad_battery_space/apps/hzb_bat_search_app.py
@@ -7,6 +7,7 @@ from nomad.config.models.ui import (
     SearchQuantities,
 )
 
+# i see a problem here, because it is only referencing one type of batteries. not used currently
 schema = (
     'nomad_battery_space.schema_packages.battery_cell_assembly_package.CoinCellBattery'
 )
@@ -17,18 +18,22 @@ hzb_bat_search_app = App(
     category='battery space',
     search_quantities=SearchQuantities(
         include=[
-            '*#nomad_battery_space.schema_packages.battery_cell_assembly_package.CoinCellBattery'
+            '*#nomad_battery_space.schema_packages.battery_cell_assembly_package.CoinCellBattery',
+            '*#nomad_battery_space.schema_packages.battery_cell_assembly_package.CylindricalCellBattery',
+            '*#nomad_battery_space.schema_packages.battery_cell_assembly_package.PouchCellBattery',
         ]
     ),
-    filters_locked={'section_defs.definition_qualified_name': [schema]},
+    # filters_locked={'section_defs.definition_qualified_name': [schema]},
     columns=[
         Column(quantity='entry_id', selected=True),
         Column(quantity='entry_name', selected=True),
-        Column(quantity=f'data.pressure#{schema}', selected=True),
-        Column(
-            quantity=f'data.case_crimp#{schema}',
-            selected=True,
-        ),
+        Column(quantity='entry_type', selected=True),
+        # This is CoinCell specific
+        # Column(quantity=f'data.pressure#{schema}', selected=True),
+        # Column(
+        #     quantity=f'data.case_crimp#{schema}',
+        #     selected=True,
+        # ),
     ],
     menu=Menu(
         title='Filter',
@@ -37,6 +42,11 @@ hzb_bat_search_app = App(
             MenuItemTerms(
                 search_quantity='authors.name',
                 title='Author',
+                options=10,
+            ),
+            MenuItemTerms(
+                search_quantity='entry_type',
+                title='Entry Type',
                 options=10,
             ),
             MenuItemVisibility(),
