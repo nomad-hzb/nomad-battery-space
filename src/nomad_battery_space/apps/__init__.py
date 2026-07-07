c97b6b2727d09b8c970ef2c8a43af021ddc24d28
Marcus Lewerenz <152180836+marlewe@users.noreply.github.com>
2026-07-07 11:29:41 +0200

    removed 2nd search app

--- src/nomad_battery_space/apps/__init__.py
+++ src/nomad_battery_space/apps/__init__.py
@@ -2,7 +2,6 @@ from nomad.config.models.plugins import AppEntryPoint
 from nomad.config.models.ui import App, Column, Columns, FilterMenu, FilterMenus
 
 from nomad_battery_space.apps.hzb_bat_search_app import hzb_bat_search_app
-from nomad_battery_space.apps.hzb_coincell_search_app import hzb_coincell_search_app
 
 app_entry_point = AppEntryPoint(
     name='NewApp',
@@ -31,12 +30,4 @@ hzb_bat_search_app_entry_point = AppEntryPoint(
     This app allows you to search HZB battery data within NOMAD.
     """,
     app=hzb_bat_search_app,
-)
-
-hzb_coincell_search_app_entry_point = AppEntryPoint(
-    name='HZB CoinCell search app',
-    description="""
-    This app allows you to search HZB CoinCell data within NOMAD.
-    """,
-    app=hzb_coincell_search_app,
 )
