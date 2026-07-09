bf5579e6914a7a8ad28dda8a3c440f4271927364
Marcus Lewerenz <152180836+marlewe@users.noreply.github.com>
2026-07-09 14:56:47 +0200

    added several filters

--- src/nomad_battery_space/apps/hzb_bat_search_app.py
+++ src/nomad_battery_space/apps/hzb_bat_search_app.py
@@ -6,6 +6,7 @@ from nomad.config.models.ui import (
     MenuItemCustomQuantities,
     MenuItemHistogram,
     MenuItemOptimade,
+    MenuItemPeriodicTable,
     MenuItemTerms,
     MenuItemVisibility,
     SearchQuantities,
@@ -38,8 +39,22 @@ for class_name in classes_with_similar_properties:
                     search_quantity=f'data.dimensions_and_weights.mass#nomad_battery_space.schema_packages.hzb_bs_package.{class_name}'
                 ),
             ),
+            MenuItemTerms(
+                search_quantity=f'data.product_info.supplier#nomad_battery_space.schema_packages.hzb_bs_package.{class_name}',
+                title='Supplier',
+                options=10,
+            ),
         ],
     )
+
+    if class_name == 'ElectrodeSheet':
+        menu.items.append(
+            MenuItemTerms(
+                search_quantity=f'data.casting_procedure#nomad_battery_space.schema_packages.hzb_bs_package.{class_name}',
+                title='Casting Procedure',
+                options=10,
+            ),
+        )
 
     subclass_filter_menus[class_name] = menu
 
@@ -56,6 +71,7 @@ hzb_bat_search_app = App(
             '*#nomad_battery_space.schema_packages.hzb_bs_package.Electrode',
             '*#nomad_battery_space.schema_packages.hzb_bs_package.ElectrodeSheet',
             '*#nomad_battery_space.schema_packages.hzb_bs_package.ElectrodeSample',
+            '*#nomad_battery_space.schema_packages.hzb_bs_package.BS_Chemical'
             # '*#nomad_battery_space.schema_packages.hzb_bs_package.DimensionsAndWeights',
             '*#nomad_battery_space.schema_packages.hzb_bs_package.SeparatorStock'
             '*#nomad_battery_space.schema_packages.hzb_bs_package.SeparatorSample'
@@ -84,12 +100,37 @@ hzb_bat_search_app = App(
                 ],
             ),
             Menu(
-                title='Entry Type',
+                title='Entry Properties',
                 items=[
                     MenuItemTerms(
                         search_quantity='entry_type',
                         title='Entry Type',
                         options=10,
+                    ),
+                    MenuItemTerms(
+                        search_quantity='entry_name',
+                        title='Entry Name',
+                        options=5,
+                    ),
+                    MenuItemHistogram(
+                        title='Create Time',
+                        x=Axis(search_quantity='entry_create_time'),
+                    ),
+                    MenuItemTerms(
+                        search_quantity='results.eln.lab_ids',
+                        title='Lab ID',
+                        options=5,
+                    ),
+                ],
+            ),
+            Menu(
+                title='Elements',
+                width='36',
+                items=[
+                    MenuItemPeriodicTable(
+                        search_quantity='results.material.elements',
+                        title='Elements',
+                        width='36',
                     ),
                 ],
             ),
@@ -97,6 +138,16 @@ hzb_bat_search_app = App(
             subclass_filter_menus['ElectrodeSample'],
             subclass_filter_menus['SeparatorStock'],
             subclass_filter_menus['SeparatorSample'],
+            Menu(
+                title='Chemical Properties',
+                items=[
+                    MenuItemTerms(
+                        search_quantity='data.product_info.supplier#nomad_battery_space.schema_packages.hzb_bs_package.BS_Chemical',
+                        title='Supplier',
+                        options=10,
+                    ),
+                ],
+            ),
             MenuItemOptimade(title='Optimade'),
             MenuItemVisibility(),
             MenuItemCustomQuantities(title='Custom Conditions'),
