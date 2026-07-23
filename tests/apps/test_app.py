38c97848afa1ef38b8cca26ceb28b6df0867cfa6
SvetiGrin <svetlana.grinman@helmholtz-berlin.de>
2026-07-23 16:06:45 +0200

    pytest fix

--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -2,4 +2,4 @@ def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
     from nomad_battery_space.apps import hzb_bat_search_app_entry_point
 
-    assert hzb_bat_search_app_entry_point.app.label == 'HZB Batteries Search App'
+    assert hzb_bat_search_app_entry_point.app.label == 'BS ELN'
